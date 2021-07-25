281791b918c84f4a03c2152dbcce57e2bf449447
Simon Lefebvre <lefs3303@usherbrooke.ca>
2021-07-25 15:22:48 -0400

    Function eplliptique

--- Python/FilterFunction.py
+++ Python/FilterFunction.py
@@ -4,7 +4,7 @@ from scipy import signal
 
 
 
-def MyFilter(N, fe, fc, name, printName, printOption):
+def MyFIRFilter(N, fe, fc, name, printName, printOption):
 
     fir_h: np.ndarray = signal.firwin(numtaps=N, cutoff=fc, pass_zero=name, window="blackman", fs=fe)
     fir_h_dft_tf: np.ndarray = np.abs(np.fft.fft(fir_h))
@@ -32,3 +32,20 @@ def MyFilter(N, fe, fc, name, printName, printOption):
         plt.subplot(2,1,2)
         nHz = np.arange(-fe/2,fe/2,fe/N)
         plt.plot(nHz,fir_H)
+
+
+def MyIIRFilter(N, PB_Gain, SB_Gain, wn, fe, Type, printName, printOption):
+
+    [b, a] = signal.ellip(N=N, rp=PB_Gain, rs=SB_Gain, Wn=wn, fs=fe, btype=Type, output="ba")
+    #[b, a] = signal.ellip(N=4, rp=1, rs=70, Wn=[950, 1050], fs=20000, btype="bandstop", output="ba")
+    [w, h_dft] = signal.freqz(b, a, worN=10000, fs=fe)
+
+    if (printOption>0):
+        plt.figure()
+        plt.plot(w, 20 * np.log10(np.abs(h_dft)))
+        plt.title("IIRFilter " + printName)
+        plt.xlabel("Fréquence [Hz]")
+        plt.ylabel("Gain [dB]")
+        plt.grid(which="both", axis="both")
+        plt.tight_layout()
+
--- Python/main.py
+++ Python/main.py
@@ -4,9 +4,10 @@ from scipy import signal
 import FilterFunction as ff
 
 
-ff.MyFilter(256,20000,500,"lowpass", "LowPass",1)
-ff.MyFilter(255,20000,[500, 1500],"bandpass","BandPass#1",1)
-ff.MyFilter(255,20000,[1500, 2500],"bandpass","BandPass#2",1)
-ff.MyFilter(255,20000,[2500, 4500],"bandpass","BandPass#3",1)
-ff.MyFilter(255,20000,4490,"highpass","highpass",1)
+ff.MyFIRFilter(256, 20000, 500, "lowpass", "LowPass", 0)
+ff.MyFIRFilter(255, 20000, [500, 1500], "bandpass", "BandPass#1", 0)
+ff.MyFIRFilter(255, 20000, [1500, 2500], "bandpass", "BandPass#2", 0)
+ff.MyFIRFilter(255, 20000, [2500, 4500], "bandpass", "BandPass#3", 0)
+ff.MyFIRFilter(255, 20000, 4490, "highpass", "highpass", 0)
+ff.MyIIRFilter(N=4, PB_Gain=1, SB_Gain=70, wn=[950, 1050], fe=20000, Type="bandstop", printName="bandstop", printOption=1)
 plt.show()
